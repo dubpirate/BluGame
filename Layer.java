c03fffa5286be7cc92f9ca719fa25a294bc05c03
Spaxsta <devonkennedynz@yahoo.co.nz>
2018-04-21 12:30:38 +1200

    Add files via upload

--- Layer.java
+++ Layer.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.io.IOException;
+import java.util.concurrent.ThreadLocalRandom;
 
 import org.newdawn.slick.Image;
 import org.newdawn.slick.Input;
@@ -9,9 +10,7 @@ import org.newdawn.slick.SlickException;
 import org.newdawn.slick.opengl.Texture;
 import org.newdawn.slick.opengl.TextureLoader;
 import org.newdawn.slick.util.ResourceLoader;
-
-import Items.Item;
-
+import org.newdawn.slick.tests.xml.Item;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 import org.lwjgl.opengl.GL11;
@@ -22,28 +21,40 @@ public class Layer {
 
 	private Texture tile;
 	private int drawSize;
-	private int height;
-	private int width;
+	private int height; // screen height
+	private int width; //  screen width
 	private int menuWidth = 100;
 	private int level; // which layer in the stack is this
-	private ArrayList<? extends Item> items;
-	private int[] stairsUp;
-	private int[] stairsDown;
+	private ArrayList<? extends Item> items; // all the items in this layer
+	private Stairs stairsUp;    // Stairs objects
+	private Stairs stairsDown; //
 	Map<String, Texture> bgTextures = new HashMap<String, Texture>();
 	private final String[] textures = { "botRight", "botWall", "Ground", "leftWall", "rightWall", "topLeft", "topRight",
-			"topWall", "botLeft", "StairsupLeft", "StairsupRight" };
+			"topWall", "botLeft"};
 
-	Layer(int level, String tileFile, ArrayList<? extends Item> items, int width, int height) throws IOException {
+	Layer(Layer prev, int level, String tileFile, ArrayList<? extends Item> items, int width, int height) throws IOException {
 		this.level = level;
 		this.items = items;
 		this.height = height;
 		this.width = width;
 		this.drawSize = height/2-menuWidth;
+		
+		if (prev != null) {
+			try {
+			stairsDown = new Stairs(prev.getStairsUp().getCoords(), "res/Layer" + level + "/Stairsup", TILESIZEWIDTH, TILESIZEHEIGHT);
+			} catch (java.lang.NullPointerException e) {
+				System.out.println("Error loading stairs on Layer" + level);
+				e.printStackTrace();
+			}
+		}
+		
+		if (level != 5) {
+			stairsUp = newStairsUp();
+		}
 
 		for (int i = 0; i < textures.length; i++) {
 			String tileName = textures[i];
-			tile = TextureLoader.getTexture("PNG",
-					ResourceLoader.getResourceAsStream(tileFile + tileName + ".PNG"));
+			tile = TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream(tileFile + tileName + ".png"));
 			bgTextures.put(tileName, tile);
 		}
 	}
@@ -51,11 +62,42 @@ public class Layer {
 	public int getLevel() {
 		return this.level;
 	}
+	
+	public Stairs getStairsUp() {
+		return this.stairsUp;
+	}
+	
+	
+	/**
+	 * this method looks for a random place in the room to put the stairs.
+	 * First: Choose a random x, y between the bounds of the screen,
+	 * Second: make sure it doesn't overlap the other stairs or the items
+	 *  --( if it does, pick a different random x, y)
+	 * Third: return the random x,y
+	 */
+	public Stairs newStairsUp() {
+		int[] newStairs = new int[2];
+		boolean collision = true;
+		do {
+			collision = false;
+			newStairs[0] = ThreadLocalRandom.current().nextInt(0+TILESIZEWIDTH, drawSize-TILESIZEWIDTH + 1);
+			newStairs[1] = ThreadLocalRandom.current().nextInt(0+TILESIZEHEIGHT, drawSize-TILESIZEHEIGHT + 1);
+			
+			// for (item in items)
+				// if collisions with x, y
+					// collision = true;
+					// break;
+				//
+			//
+		} while (collision);
+		
+		return new Stairs(newStairs, "res/Layer" + level + "/Stairsup", TILESIZEWIDTH, TILESIZEHEIGHT);
+	}
 
 	public void draw() {
-		drawLayer();
-		drawStairs();
-		drawItems();
+		drawLayer();     // first draw the floor and walls
+		drawStairs();   //  then the stairs
+		drawItems();   //   then the items
 	}
 
 	private void drawLayer() {
@@ -235,39 +277,15 @@ public class Layer {
 	}
 
 	private void drawStairs() {
+		// this draws the stairs down, if there are any.
+		if (level != 1) {
+			stairsDown.draw();
+		}
 		
-		bgTextures.get("StairsupLeft").bind();
-		GL11.glBegin(GL11.GL_QUADS);
-
-		GL11.glVertex2f(drawSize/2-TILESIZEWIDTH/4, TILESIZEHEIGHT);
-		GL11.glTexCoord2f(0, 0);
-
-		GL11.glVertex2f(drawSize/2-TILESIZEWIDTH/4, TILESIZEHEIGHT + TILESIZEHEIGHT);
-		GL11.glTexCoord2f(1, 0);
-
-		GL11.glVertex2f(drawSize/2-TILESIZEWIDTH/4 + TILESIZEWIDTH, TILESIZEHEIGHT + TILESIZEHEIGHT);
-		GL11.glTexCoord2f(1, 1);
-
-		GL11.glVertex2f(drawSize/2-TILESIZEWIDTH/4 + TILESIZEWIDTH, TILESIZEHEIGHT);
-		GL11.glTexCoord2f(0, 1);
-
-		GL11.glEnd();
-		bgTextures.get("StairsupRight").bind();
-		GL11.glBegin(GL11.GL_QUADS);
-
-		GL11.glVertex2f(drawSize/2+TILESIZEWIDTH-2-TILESIZEWIDTH/4, TILESIZEHEIGHT);
-		GL11.glTexCoord2f(0, 0);
-
-		GL11.glVertex2f(drawSize/2+TILESIZEWIDTH-2-TILESIZEWIDTH/4, TILESIZEHEIGHT + TILESIZEHEIGHT);
-		GL11.glTexCoord2f(1, 0);
-
-		GL11.glVertex2f(drawSize/2 + TILESIZEWIDTH+TILESIZEWIDTH-2-TILESIZEWIDTH/4, TILESIZEHEIGHT + TILESIZEHEIGHT);
-		GL11.glTexCoord2f(1, 1);
-
-		GL11.glVertex2f(drawSize/2 + TILESIZEWIDTH+TILESIZEWIDTH-2-TILESIZEWIDTH/4, TILESIZEHEIGHT);
-		GL11.glTexCoord2f(0, 1);
-
-		GL11.glEnd();
+		// this draws the stairs down, if there are any.
+		if (level != 5) {
+			stairsUp.draw();
+		}
 		
 	}
 

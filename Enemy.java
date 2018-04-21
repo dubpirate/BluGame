40a515cf24529b340fe2def9c303d917d862479c
blackmcurt <curtis.blackman@outlook.com>
2018-04-22 06:10:10 +1200

    Add files via upload

--- Enemy.java
+++ Enemy.java
@@ -9,6 +9,7 @@ import org.newdawn.slick.SlickException;
 
 import Aesthetic.Rock;
 import Interactable.Chest;
+import Items.Item;
 
 import java.lang.*;
 
@@ -21,6 +22,10 @@ public class Enemy {
 	private int tileHeight;
 	private boolean dead = false;
 	Image img;
+	private boolean canLeft;
+	private boolean canRight;
+	private boolean canTop;
+	private boolean canBottom;
 
 	public Enemy( Player player, Layer currentLayer, int tileWidth, int tileHeight) throws SlickException {
 		this.player = player;
@@ -43,6 +48,18 @@ public class Enemy {
 				if (currentLayer.getContents().get(i) instanceof Rock || currentLayer.getContents().get(i) instanceof Chest) {
 					int[] itemCoords = currentLayer.getContents().get(i).getCoords();
 					if (x==itemCoords[0]/tileWidth && y==itemCoords[1]/tileHeight) {
+						flag = false;
+					}
+				}
+				if (currentLayer.getLevel()>1) {
+					int[] stairsDownCoords = currentLayer.getStairsDown().getCoords();
+					if (stairsDownCoords[0]/tileWidth==x && stairsDownCoords[1]/tileHeight==y) {
+						flag = false;
+					}
+				}
+				if (currentLayer.getLevel()<20) {
+					int[] stairsUpCoords = currentLayer.getStairsUp().getCoords();
+					if (stairsUpCoords[0]/tileWidth==x && stairsUpCoords[1]/tileHeight==y) {
 						flag = false;
 					}
 				}
@@ -85,34 +102,22 @@ public class Enemy {
 		if (currentLayer.getLevel()==player.getCurrentLayer().getLevel()) {
 			int dx = x - player.getX();
 			int dy = y - player.getY();
-			if (dx == 0 && dy== 0) {
-				if (x<9) {
-					x++;
-					img = new Image("res/Sprites/ShadowLeft.png");
-					//attack here
-					return;
-				} else if(y<10) {
-					y++;
-					img = new Image("res/Sprites/ShadowFront.png");
-					//attack here
-					return;
-				}
-			}
+			checkSurroundings();
 			if (dx==1 && dy==0) {
 				img = new Image("res/Sprites/ShadowLeft.png");
-				//attack here
+				hug();
 				return;
 			}else if (dx==-1 && dy==0) {
 				img = new Image("res/Sprites/ShadowRight.png");
-				//attack here
+				hug();
 				return;
 			}else if (dy==-1 && dx==0) {
 				img = new Image("res/Sprites/ShadowBack.png");
-				//attack here
+				hug();
 				return;
 			}else if (dy==1 && dx==0) {
 				img = new Image("res/Sprites/ShadowFront.png");
-				//attack here
+				hug();
 				return;
 			}
 			int max = dx;
@@ -122,20 +127,70 @@ public class Enemy {
 
 			if (max == dy) {
 				if (dy < 0) {
-					y++;
+					if (canTop) {
+						y++;
+					}
 					img = new Image("res/Sprites/ShadowBack.png");
 				} else {
-					y--;
+					if (canBottom) {
+						y--;
+					}
 					img = new Image("res/Sprites/ShadowFront.png");
 				}
 			} else if (max == dx) {
 				if (dx < 0) {
-					x++;
+					if (canRight) {
+						x++;
+					}
 					img = new Image("res/Sprites/ShadowRight.png");
 				} else {
-					x--;
+					if (canLeft) {
+						x--;
+					}
 					img = new Image("res/Sprites/ShadowLeft.png");
 				}
+			}
+		}
+	}
+	
+	public void hug() {
+		player.setHealth(-1);
+	}
+	
+	public void checkSurroundings() {
+		canLeft = true;
+		canRight = true;
+		canTop = true;
+		canBottom = true;
+		for(Item i: currentLayer.getContents()) {
+			if (i instanceof Rock || i instanceof Chest) {
+				int[] coords = i.getCoords();
+				if(coords[0]/tileWidth==x-1 && coords[1]/tileHeight==y) {
+					canLeft = false;
+				}
+				if(coords[0]/tileWidth==x+1 && coords[1]/tileHeight==y) {
+					canRight = false;
+				}
+				if(coords[0]/tileWidth==x && coords[1]/tileHeight==y+1) {
+					canTop = false;
+				}
+				if(coords[0]/tileWidth==x && coords[1]/tileHeight==y-1) {
+					canBottom = false;
+				}
+			}
+		}
+		for(Enemy e:player.getEnemyList()) {
+			if (e.getX()==x-1 && e.getY()==y && e.getLayer().getLevel()==currentLayer.getLevel() && !e.getDead()) {
+				canLeft = false;
+			}
+			if (e.getX()==x+1 && e.getY()==y && e.getLayer().getLevel()==currentLayer.getLevel() && !e.getDead()) {
+				canRight = false;
+			}
+			if (e.getX()==x && e.getY()==y+1 && e.getLayer().getLevel()==currentLayer.getLevel() && !e.getDead()) {
+				canTop = false;
+			}
+			if (e.getX()==x && e.getY()==y-1 && e.getLayer().getLevel()==currentLayer.getLevel() && !e.getDead()) {
+				canBottom = false;
 			}
 		}
 	}
@@ -150,5 +205,9 @@ public class Enemy {
 	
 	public Layer getLayer() {
 		return currentLayer;
+	}
+	
+	public boolean getDead() {
+		return dead;
 	}
 }

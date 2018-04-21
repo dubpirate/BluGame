2d6e9e11a81dc0d8f6379c6944e734585ffa92cb
dubpirate <w@torkington.com>
2018-04-21 20:28:18 +1200

    More aesthetics

---
+++ Rock.java
@@ -0,0 +1,10 @@
+package Aesthetic;
+
+import Items.Aesthetic;
+
+public class Rock extends Aesthetic {
+
+	public Rock() {
+		super("res/Aesthetic/Rock.png", null);
+	}
+}
---
+++ Rubble.java
@@ -0,0 +1,9 @@
+package Aesthetic;
+
+import Items.Aesthetic;
+
+public class Rubble extends Aesthetic {
+	public Rubble() {
+		super("res/Aesthetic/Rubble.png", null);
+	}
+}

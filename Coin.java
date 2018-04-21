9634247001e9f42d44cc71691dde612686db1d87
dubpirate <w@torkington.com>
2018-04-22 06:50:38 +1200

    Removed useless stuff

--- Coin.java
+++ Coin.java
@@ -3,20 +3,7 @@ package Interactable;
 import Items.*;
 
 public class Coin extends Aesthetic {
-	private int code;
-	private static String name = "Coin";
-	public Coin(int code) {
-		super("res/Interactables/Gold.png", null, name);	
-		this.code = code;
+	public Coin() {
+		super("res/Interactables/Coin.png", null);
 	}
-	
-	public int getCode(){
-		return code;
-	}
-	
-	public String getName(){
-		return name;
-	}
-	// later, add some methods about unlocking when person with key comes by.
-	
 }
